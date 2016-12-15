6925b468df050cbcc16886a334f3224c6d0da857
sun06 <sun06@SUN-PC-4>
2016-12-15 17:07:08 +0800

    test github plugin

--- Test/initialization.h
+++ Test/initialization.h
@@ -3,9 +3,6 @@
 namespace TestInitialization {
 	void test_list_init() {
 		//int a{ 3.14f }; // precision loss check is strict
-		const int &ref = 4 * 2;
-		int &i = const_cast<int&>(ref);
-		i = 42;
 	}
 	void test_default_init() {
 
